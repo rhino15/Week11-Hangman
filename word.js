b4ccac4f07ea965267203502c13449c39caf7832
Roy Hinojosa <roy.hinojosa@gmail.com>
2016-07-05 21:43:17 -0500

    made changes to word.js

--- word.js
+++ word.js
@@ -4,24 +4,35 @@ function CheckWord(letterGuessed, randomWord) {
 	this.letterGuessed = letterGuessed;
 	this.randomWord = randomWord;
 	this.correctGuessArr = [];
+	this.incorrectGuessArr = [];
 	this.noDuplicatesArr = [];
 
-	this.checkGuess = function() {
+
+	this.checkCorrectGuess = function() {
 		for (var i = 0; i < this.randomWord.length; i++) {
 			if (this.letterGuessed === this.randomWord.charAt(i)) {
 				this.correctGuessArr.push(this.letterGuessed);
 				return true;
 			} else {
+				this.incorrectGuessArr.push(this.letterGuessed);
 				return false;
 			}
 		}
 	};
 	//ween
-	this.noDuplicates = function() {
-		for (var i = 0; i < this.correctGuessArr.length - 1; i++) {
+	this.noDuplicatesCorrectArr = function() {
+		for (var i = 0; i < this.correctGuessArr.length; i++) {
 			if (this.noDuplicatesArr.indexOf(this.correctGuessArr[i]) === -1) {
 				this.noDuplicatesArr.push(this.correctGuessArr[i]);
 			}	
+		}
+	};
+
+	this.noDuplicatesIncorrectArr = function() {
+		for (var i = 0; i < this.incorrectGuessArr.length; i++) {
+			if (this.noDuplicatesArr.indexOf(this.incorrectGuessArr[i]) === -1) {
+				this.noDuplicatesArr.push(this.incorrectGuessArr[i]);
+			}
 		}
 	};
 
