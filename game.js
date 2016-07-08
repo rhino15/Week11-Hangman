18f45d35d81d5b13f6a53310f5d9e3916a42a1a1
Roy Hinojosa <roy.hinojosa@gmail.com>
2016-07-08 17:58:18 -0500

    uncommented game.js

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
 function WordBank() {
-	//this.wordBank = ['ween', 'queens of the stone age', 'bruce springsteen', 'tom waits', 'queen', 'diahrrea planet', 'howlin wolf', 'jimi hendrix', 'sigur ros', 'the beatles'];
-	this.wordBank = ['ween'];
+	this.wordBank = ['ween', 'queens of the stone age', 'bruce springsteen', 'tom waits', 'queen', 'diahrrea planet', 'howlin wolf', 'jimi hendrix', 'sigur ros', 'the beatles'];
 }
 
 module.exports = WordBank;
